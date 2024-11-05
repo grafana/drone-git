8656e1aea1d17bb93ef08eb8385faba8b9571600
Ricky Whitaker <ricky.whitaker@grafana.com>
2024-11-05 14:39:56 -0600

    fix(clone-pwsh): refactor other windows scripts to avoid empty flag (#5)
    * Updated Windows clone scripts to handle env vars differently
    
    * fix: Set log output to match actual command being run
    
    * fix: Set log output to match actual command being run

--- windows/clone-commit.ps1
+++ windows/clone-commit.ps1
@@ -1,7 +1,8 @@
-
-Set-Variable -Name "FLAGS" -Value ""
 if ($Env:PLUGIN_DEPTH) {
-    Set-Variable -Name "FLAGS" -Value "--depth=$Env:PLUGIN_DEPTH" 
+    $flags = "--depth=$Env:PLUGIN_DEPTH"
+}
+else {
+    $flags = ""
 }
 
 if (!(Test-Path .git)) {
@@ -11,7 +12,14 @@ if (!(Test-Path .git)) {
 	git remote add origin $Env:DRONE_REMOTE_URL
 }
 
-Write-Host "git fetch $FLAGS origin +refs/heads/${Env:DRONE_COMMIT_BRANCH}:";
-git fetch $FLAGS origin "+refs/heads/${Env:DRONE_COMMIT_BRANCH}:";
-Write-Host "git checkout $Env:DRONE_COMMIT_SHA -f $Env:DRONE_COMMIT_BRANCH";
+if ($flags) {
+    Write-Host "git fetch $flags origin +refs/heads/${Env:DRONE_COMMIT_BRANCH}:";
+    git fetch $flags origin "+refs/heads/${Env:DRONE_COMMIT_BRANCH}:";
+}
+else {
+    Write-Host "git fetch origin +refs/heads/${Env:DRONE_COMMIT_BRANCH}:";
+    git fetch origin "+refs/heads/${Env:DRONE_COMMIT_BRANCH}:";
+}
+
+Write-Host "git checkout $Env:DRONE_COMMIT_SHA -b $Env:DRONE_COMMIT_BRANCH;";
 git checkout $Env:DRONE_COMMIT_SHA -b $Env:DRONE_COMMIT_BRANCH;
